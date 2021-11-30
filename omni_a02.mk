93375e9f9f8f28ca8f9359131963d47b16cfaaa3
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2021-11-30 08:36:16 +0800

    Update omni_a02.mk

--- omni_a02.mk
+++ omni_a02.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 
 # Inherit from a02 Device
